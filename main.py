6397a8462c2977e76403672466051359c1a0bdc3
Michael Rogness <mike@rogness.net>
2024-05-29 19:25:28 -0500

    Added Board Class Framework and Board and Square dimensions

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ pygame.init()
 
 
 class Piece:
-    def __init__(self):
-        pass
+    def __init__(self, FEN: str, color: str, board_pos: tuple[int]):
+        self.FEN = FEN
+        self.color = color
+        self.board_pos = board_pos
 
 
 class Move:
@@ -15,16 +17,34 @@ class Move:
         self.target_square = target_square
 
 
+class Board:
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+        
+        self.size = (width, height)
+        
+    
+    def Draw(self):
+        pass
 
 
 
 
-window_size = (600, 600)
+window_size = (300, 300)
 window_width, window_height = window_size
 window_name = "Chess V3"
 
 window = pygame.display.set_mode(window_size)
 pygame.display.set_caption(window_name)
+
+
+board_size = (8, 8)
+board_width, board_height = board_size
+
+square_width = window_width/board_width
+square_height = window_height/board_height
+square_size = (square_width, square_height)
 
 while True:
     for event in pygame.event.get():
