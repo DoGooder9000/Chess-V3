518088dda5f995ae24ea48e73df47aad43b81c2b
Michael Rogness <mike@rogness.net>
2024-05-29 19:34:37 -0500

    Added the Board Class Draw Function and colors. Imported piece pictures too

--- main.py
+++ main.py
@@ -26,10 +26,16 @@ class Board:
         
     
     def Draw(self):
-        pass
+        for x in range(board_width):
+            for y in range(board_height):
+                if (x+y)%2 == 0:
+                    pygame.draw.rect(window, LightSquareColor, pygame.Rect((x*square_width, y*square_height), square_size))
+                else:
+                    pygame.draw.rect(window, DarkSquareColor, pygame.Rect((x*square_width, y*square_height), square_size))
 
 
-
+def Update():
+    pygame.display.update()
 
 window_size = (300, 300)
 window_width, window_height = window_size
@@ -45,10 +51,41 @@ board_width, board_height = board_size
 square_width = window_width/board_width
 square_height = window_height/board_height
 square_size = (square_width, square_height)
+                                     
+LightSquareColor = (209, 162, 96)
+DarkSquareColor = (115, 90, 56)
+
+
+WhiteKing = pygame.transform.scale(pygame.image.load('Images/whiteking.png'), square_size)
+BlackKing = pygame.transform.scale(pygame.image.load('Images/blackking.png'), square_size)
+
+WhiteQueen = pygame.transform.scale(pygame.image.load('Images/whitequeen.png'), square_size)
+BlackQueen = pygame.transform.scale(pygame.image.load('Images/blackqueen.png'), square_size)
+
+WhiteKnight = pygame.transform.scale(pygame.image.load('Images/whiteknight.png'), square_size)
+BlackKnight = pygame.transform.scale(pygame.image.load('Images/blackknight.png'), square_size)
+
+WhiteBishop = pygame.transform.scale(pygame.image.load('Images/whitebishop.png'), square_size)
+BlackBishop = pygame.transform.scale(pygame.image.load('Images/blackbishop.png'), square_size)
+
+WhiteRook = pygame.transform.scale(pygame.image.load('Images/whiterook.png'), square_size)
+BlackRook = pygame.transform.scale(pygame.image.load('Images/blackrook.png'), square_size)
+
+WhitePawn = pygame.transform.scale(pygame.image.load('Images/whitepawn.png'), square_size)
+BlackPawn = pygame.transform.scale(pygame.image.load('Images/blackpawn.png'), square_size)
+
+
+
+Board = Board(8, 8)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+    
+    window.fill('black')
+    
+    Board.Draw()
+    Update()
 
